f06bb5fd8db050effb5bf981627fe2b976c140d7
Calvin Tam <51683307+ctam4@users.noreply.github.com>
2019-09-12 10:02:13 -0700

    Add support for handling (-X) in Evaluator

--- calculator/src/main/java/edu/csc413/calculator/evaluator/Evaluator.java
+++ calculator/src/main/java/edu/csc413/calculator/evaluator/Evaluator.java
@@ -43,61 +43,76 @@ public class Evaluator {
     }
 
     public int eval(String expression) {
-        String token;
+        String token = "", lastToken = token;
         Operand op1, op2;
+        boolean nextOpNeg = false;
 
         // The 3rd argument is true to indicate that the delimiters should be used
         // as tokens, too. But, we'll need to remember to filter out spaces.
         this.tokenizer = new StringTokenizer(expression, DELIMITERS, true);
 
-        while (this.tokenizer.hasMoreTokens()) {
-            // filter out spaces
+        while (this.tokenizer.hasMoreTokens()) {// filter out spaces
             if (!(token = this.tokenizer.nextToken()).equals(" ")) {
                 try {
                     // check if token is an operand
                     if (Operand.check(token)) {
-                        operandStack.push(new Operand(token));
+                        // check if this operand should be negative
+                        if (nextOpNeg) {
+                            operandStack.push(new Operand("-" + token));
+                            nextOpNeg = false;
+                        } else {
+                            operandStack.push(new Operand(token));
+                        }
                     } else {
                         // throw exception if token is not an operator
                         if (!Operator.check(token)) {
                             System.out.println("*****invalid token******");
                             throw new RuntimeException("*****invalid token******");
                         }
-                        // create new operator object from token
-                        Operator newOperator = Operator.getOperator(token);
-                        // check if operator is "(", operator stack is empty, or not empty but current operator priority is greater than the previous one
-                        if (newOperator.getClass().getSimpleName().equals("OpeningParenthesisOperator") ||
-                        operatorStack.isEmpty() ||
-                        newOperator.priority() >= operatorStack.peek().priority()) {
-                            operatorStack.push(newOperator);
+                        // check if previous operator is a (, so next operand is a negative value because (-
+                        if (token.equals("-") &&
+                        lastToken.equals("(")) {
+                            nextOpNeg = true;
                         }
-                        // check if operator is ")"
-                        else if (newOperator.getClass().getSimpleName().equals("ClosingParenthesisOperator")) {
-                            // process operators until the "(" is encountered
-                            do {
-                                op2 = operandStack.pop();
-                                op1 = operandStack.pop();
-                                process(operatorStack.pop(), op1, op2);
-                            } while (!operatorStack.peek().getClass().getSimpleName().equals("OpeningParenthesisOperator"));
-                            operatorStack.pop();
-                        }
-                        // if none of above cases apply
+                        // if expression is not (-
                         else {
-                            // process an operator only if operator stack is not empty and top of operator stack has higher priority
-                            while (!operatorStack.isEmpty() &&
-                            operatorStack.peek().priority() >= newOperator.priority() &&
-                            operandStack.size() >= 2) {
-                                op2 = operandStack.pop();
-                                op1 = operandStack.pop();
-                                process(operatorStack.pop(), op1, op2);
+                            // create new operator object from token
+                            Operator newOperator = Operator.getOperator(token);
+                            // check if operator is "(", operator stack is empty, or not empty but current operator priority is greater than the previous one
+                            if (newOperator.getClass().getSimpleName().equals("OpeningParenthesisOperator") ||
+                            operatorStack.isEmpty() ||
+                            newOperator.priority() > operatorStack.peek().priority()) {
+                                operatorStack.push(newOperator);
                             }
-                            operatorStack.push(newOperator);
+                            // check if operator is ")"
+                            else if (newOperator.getClass().getSimpleName().equals("ClosingParenthesisOperator")) {
+                                // process operators until the "(" is encountered
+                                while (!operatorStack.peek().getClass().getSimpleName().equals("OpeningParenthesisOperator")) {
+                                    op2 = operandStack.pop();
+                                    op1 = operandStack.pop();
+                                    process(operatorStack.pop(), op1, op2);
+                                }
+                                operatorStack.pop();
+                            }
+                            // if none of above cases apply
+                            else {
+                                // process an operator only if operator stack is not empty and top of operator stack has higher priority
+                                while (!operatorStack.isEmpty() &&
+                                operatorStack.peek().priority() >= newOperator.priority() &&
+                                operandStack.size() >= 2) {
+                                    op2 = operandStack.pop();
+                                    op1 = operandStack.pop();
+                                    process(operatorStack.pop(), op1, op2);
+                                }
+                                operatorStack.push(newOperator);
+                            }
                         }
                     }
                 } catch (IllegalArgumentException e) {
                     System.out.println("*****null argument******");
                     throw new RuntimeException("*****null argument******");
                 }
+                lastToken = token;
             }
         }
 
